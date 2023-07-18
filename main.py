1528af8709d23f691bc1f04fcd0eaf13b9e07bf6
derek <dmonarch134@gmail.com>
2023-07-17 21:48:45 -0500

    test auth

--- main.py
+++ main.py
@@ -1,45 +1,19 @@
 import tempfile
-from datetime import datetime, timedelta
 from typing import Union
 from pydantic import BaseModel
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 import openai
 import os
-from typing import Annotated
 import requests
 import boto3
 from supabase import create_client, Client
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
-from jose import jwt
-from passlib.context import CryptContext
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseSettings
+
 
 
 
 from dotenv import load_dotenv
 load_dotenv()
-
-
-class Settings(BaseSettings):
-    SUPABASE_URL: str
-    AWS_ACCESS_KEY: str
-    OPENAI_KEY: str
-    S3_BUCKET: str
-    AWS_ACCESS_KEY: str
-    AWS_SECRET_KEY: str
-    URL: str
-    SECRET_KEY: str
-    
-    class Config:
-        env_file = '.env'
-   
-def get_settings():
-    return Settings()
-
 SupaUrl = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(SupaUrl, key)
@@ -50,41 +24,8 @@ s3_endpoint = os.environ.get('S3_BUCKET')
 aws_access_key = os.environ.get('AWS_ACCESS_KEY')
 aws_secret_key = os.environ.get('AWS_SECRET_KEY')
 
-SECRET_KEY="d8e2d3dfd513073e0e5f4a45c964cbefb60a706faae95cb33304a00d3d627570"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+app = FastAPI()
 
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-class TokenData(BaseModel):
-    username: str | None = None
-
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
-
-class UserInDB(User):
-    hashed_password: str
-
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 class Item(BaseModel):
     name: str
     price: float
@@ -93,120 +34,6 @@ class Item(BaseModel):
 class Interaction(BaseModel):
     user_id: str
     user_input: str
-
-
-app = FastAPI()
-supabase: Client = create_client(SupaUrl, key)
-
-origins = [
-    "http://localhost:3000",  # React app address
-    "http://localhost.tiangolo.com",
-    "http://your-react-app-url-or-ip", # if it's deployed
-    "http://your-fastapi-app-url-or-ip", # your fastapi app url or ip
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.post("/sign_in")
-async def sign_in(email: str, password: str):
-       response = supabase.auth.sign_in_with_password({"email": email, "password": password})
-       print(response)
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    print(user)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)]
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
-
-@app.post("/token", response_model=Token)
-async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
-):
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
-@app.get("/users/me/", response_model=User)
-async def read_users_me(
-    current_user: Annotated[User, Depends(get_current_active_user)]
-):
-    return current_user
-
-@app.get("/users/me/items/")
-async def read_own_items(
-    current_user: Annotated[User, Depends(get_current_active_user)]
-):
-    return [{"item_id": "Foo", "owner": current_user.username}]
 
 @app.post("/interact")
 async def interact(interaction: Interaction):
@@ -224,9 +51,9 @@ async def interact(interaction: Interaction):
     return {'prompt': past_interactions,}
 
 @app.get("/")
-async def read_root(settings: Settings = Depends(get_settings)):
-    print(settings.SUPABASE_URL)
-    return{"SUPABASE_URL": settings.SUPABASE_URL}
+async def read_root():
+    print(openai.api_key)
+    return {"Hello": "World"}
    
   # Initialize your ASR handler
 
@@ -296,7 +123,7 @@ async def ask2(question: str):
    print(response)
    speak = response.choices[0].message['content'].strip()
    print(speak)
-   textToSpeech(speak)
+   #textToSpeech(speak)
 
    return {"James said": speak}
 
@@ -326,9 +153,10 @@ def textToSpeech(speak: str):
             if chunk:
                 f.write(chunk)
     
-        uploadAudio(audio_file_path) 
+        #uploadAudio(audio_file_path) 
         return {"James said": speak, "file_path": audio_file_path}
-   
+
+
 def uploadAudio(file_path: str):
     counter = 1 
     session = boto3.Session(
